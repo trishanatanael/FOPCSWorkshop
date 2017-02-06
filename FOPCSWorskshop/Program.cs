e85d0551acda95a0dc853f04cbc3e2433c683e25
t <t@TRISHACN>
2017-02-06 16:15:49 +0800

    Add project files.

---
+++ FOPCSWorskshop/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOPCSWorskshop
+{
+    class WorkshopA
+    {
+        public static void Main()
+        {
+            System.Console.WriteLine("Name: Trisha Natanael");
+            System.Console.WriteLine("Email: tcnatanael@gmail.com");
+
+            string name = Console.ReadLine();
+            Console.WriteLine("Good Morning, {0}", name);
+
+            int a = 10;
+            Console.WriteLine(Math.Pow(a,2);
+
+            double b = 44.98;
+            Console.WriteLine(Math.Pow(b, 2);
+
+            double c = 16.8123;
+            Console.WriteLine("{0:0.00}",c);
+        }
+    }
+}
---
+++ FOPCSWorskshop/WorkshopB.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOPCSWorskshop
+{
+    class WorkshopB
+    {
+        public static void Main()
+        {
+            double d = 144.00;
+            Console.WriteLine(Math.Pow(d, 0.5));
+            
+        }
+    
+    }
+}
