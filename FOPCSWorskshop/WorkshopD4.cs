512109ee0d272235e3c0220655a3bfe11cafa918
trishacn <trisha.natanael@gmail.com>
2017-02-10 23:51:20 +0800

    all ok

--- FOPCSWorskshop/WorkshopD4.cs
+++ FOPCSWorskshop/WorkshopD4.cs
@@ -25,13 +25,13 @@ namespace FOPCSWorskshop
                 return;
             }
 
-             guess = random.NextDouble() * n;// random will never go beyond n and lower than 0
+             guess = random.NextDouble() * n;// random will never go beyond n and lower than 0 since it nextdouble(0,1) doesnot allow us to enter range
 
             if (guess * guess != n)
             {
                 guess = (guess + (n / guess)) / 2;
 
-                while (n - guess * guess >= 0.00001 || guess * guess - n >= 0.00001)
+                while (guess * guess - n >= 0.00001 || n - guess * guess >= 0.00001)
                 {
                     guess = (guess + (n / guess)) / 2;
                 }
