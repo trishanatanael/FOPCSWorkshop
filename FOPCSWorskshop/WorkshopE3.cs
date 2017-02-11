574309ac7b2c3df05529f85954906f64646669b5
trishacn <trisha.natanael@gmail.com>
2017-02-11 15:44:38 +0800

    E3 added

---
+++ FOPCSWorskshop/WorkshopE3.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOPCSWorskshop
+{
+    class WorkshopE3
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Please input a number: ");
+            int input = Convert.ToInt32(Console.ReadLine());
+
+            bool isPrime = true;
+            for (int i = 2; i <= input - 1; i++)
+            {
+                if (input % i == 0)
+                {
+                    Console.WriteLine("The number is a not Prime Number");
+                    isPrime = false;
+                    break;
+                }
+            }
+            if (isPrime)
+            {
+                Console.WriteLine("The number is a Prime Number");
+            }
+                   
+        }
+    }
+}
---
+++ FOPCSWorskshop/WorkshopE6.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOPCSWorskshop
+{
+    class WorkshopE6
+
+    {
+        public static void Main()
+        {
+            string[] salesMonth = new string[12] {"January","February","March",
+                                               "April","May","June","July",
+                                               "August","September","October",
+                                               "November","December"};
+            //12 rows of staements
+            int[] salesInfo = new int[12] { 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21};
+            int i=0;
+            double totalSales;
+            double aveSales;
+            for (i = 0; i < 12; i++)
+            {
+                Console.WriteLine("{0} \t\t\t {1}", salesMonth[i], salesInfo[i]);
+
+            }
+            totalSales = salesInfo.Sum();
+            Console.WriteLine(totalSales);
+
+            int maxSales = salesInfo.Max();
+            Console.WriteLine("Max sales is {0}", maxSales);
+
+            int minSales = salesInfo.Min();
+            Console.WriteLine("Min sales is {0}", minSales);
+
+            aveSales = Convert.ToDouble(totalSales /i);
+            Console.WriteLine("Average sales this year is {0}", aveSales);
+
+        }
+    }
+}
