1eabac7681ff96045b56d96696f351d15b1a522a
trishacn <trisha.natanael@gmail.com>
2017-02-14 02:53:52 +0800

    FINALLY F ALL DONE! YEAY!

--- FOPCSWorskshop/WorkshopF3.cs
+++ FOPCSWorskshop/WorkshopF3.cs
@@ -10,23 +10,32 @@ namespace FOPCSWorskshop
     {
         public static void Main()
         {
-            int[][] Marks = new int[12][];
-            Marks[0] = new int[4] { 56, 84, 68, 29 };
-            Marks[1] = new int[4] { 94, 73, 31, 96 };
-            Marks[2] = new int[4] { 41, 63, 36, 90 };
-            Marks[3] = new int[4] { 99, 9, 18, 17 };
-            Marks[4] = new int[4] { 62, 3, 65, 75 };
-            Marks[5] = new int[4] { 40, 96, 53, 23 };
-            Marks[6] = new int[4] { 81, 15, 27, 30 };
-            Marks[7] = new int[4] { 21, 70, 100, 22 };
-            Marks[8] = new int[4] { 88, 50, 13, 12 };
-            Marks[9] = new int[4] { 48, 54, 52, 78 };
-            Marks[10] = new int[4] { 64, 71, 67, 25 };
-            Marks[11] = new int[4] { 16, 93, 46, 72 };
 
-            double[] aveMarks = new double[12];
+            //use jagged arrays
 
+            // Declare the array of two elements:
+            int[][] subjects = new int[12][];
 
+            // Initialize the elements:
+            subjects[0] = new int[4] { 56, 84, 68, 29 };
+            subjects[1] = new int[4] { 94, 73, 31, 96 };
+            subjects[2] = new int[4] { 41, 63, 36, 90 };
+            subjects[3] = new int[4] { 99, 9, 18, 17 };
+            subjects[4] = new int[4] { 62, 3, 65, 75 };
+            subjects[5] = new int[4] { 40, 96, 53, 23 };
+            subjects[6] = new int[4] { 81, 15, 27, 30 };
+            subjects[7] = new int[4] { 21, 70, 100, 22 };
+            subjects[8] = new int[4] { 88, 50, 13, 12 };
+            subjects[9] = new int[4] { 48, 54, 52, 78 };
+            subjects[10] = new int[4] { 64, 71, 67, 25 };
+            subjects[11] = new int[4] { 16, 93, 46, 72 };
+
+            // Display the array elements:
+            for (int i = 0; i < subjects.Length; i++)
+            {
+                Console.WriteLine("Element {0}", i);
+
+            }
 
 
         }
---
+++ FOPCSWorskshop/WorkshopG3.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOPCSWorskshop
+{
+    class WorkshopG3
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Input a phrase: ");
+            string input = Console.ReadLine();
+            //take care of first letter first.
+            int i = 0;
+            if (i == 0 && char.IsLetter(input[0]))
+            {
+                //replace the first character
+                string a = Convert.ToString(input[0]);
+                string aUp = a.ToUpper();
+                var aStringBuilder = new StringBuilder(input);
+                aStringBuilder.Remove(0, 1);
+                aStringBuilder.Insert(0, aUp);
+                input = aStringBuilder.ToString();
+            }
+            for (i =1; i < input.Length; i++)
+            {
+                if (input[i] == ' ' && (i + 1 < input.Length) && char.IsLetter(input[i + 1]))
+                {
+                            char b = input[i + 1];
+                            string b2 = Convert.ToString(b);
+                            string bUp = b2.ToUpper();
+                            var aStringBuilder = new StringBuilder(input);
+                            aStringBuilder.Remove((i+1), 1);
+                            aStringBuilder.Insert((i+1), bUp);
+                            input = aStringBuilder.ToString();
+
+                }
+            }
+
+            Console.WriteLine("The final string: '{0}'", input);
+        }
+    }
+}
