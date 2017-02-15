ab90118bdbaa62238d12f548d74b7a4b10baef09
trishacn <trisha.natanael@gmail.com>
2017-02-16 01:36:50 +0800

    FDONE

--- FOPCSWorskshop/WorkshopF3.cs
+++ FOPCSWorskshop/WorkshopF3.cs
@@ -8,36 +8,73 @@ namespace FOPCSWorskshop
 {
     class WorkshopF3
     {
-        public static void Main()
+        static void Main(string[] args)
         {
 
-            //use jagged arrays
+            // Example for multidimensional array
 
-            // Declare the array of two elements:
-            int[][] subjects = new int[12][];
+            int[][] Marks = new int[12][];
+            Marks[0] = new int[4] { 56, 84, 68, 29 };
+            Marks[1] = new int[4] { 94, 73, 31, 96 };
+            Marks[2] = new int[4] { 41, 63, 36, 90 };
+            Marks[3] = new int[4] { 99, 9, 18, 17 };
+            Marks[4] = new int[4] { 62, 3, 65, 75 };
+            Marks[5] = new int[4] { 40, 96, 53, 23 };
+            Marks[6] = new int[4] { 81, 15, 27, 30 };
+            Marks[7] = new int[4] { 21, 70, 100, 22 };
+            Marks[8] = new int[4] { 88, 50, 13, 12 };
+            Marks[9] = new int[4] { 48, 54, 52, 78 };
+            Marks[10] = new int[4] { 64, 71, 67, 25 };
+            Marks[11] = new int[4] { 16, 93, 46, 72 };
+            
 
-            // Initialize the elements:
-            subjects[0] = new int[4] { 56, 84, 68, 29 };
-            subjects[1] = new int[4] { 94, 73, 31, 96 };
-            subjects[2] = new int[4] { 41, 63, 36, 90 };
-            subjects[3] = new int[4] { 99, 9, 18, 17 };
-            subjects[4] = new int[4] { 62, 3, 65, 75 };
-            subjects[5] = new int[4] { 40, 96, 53, 23 };
-            subjects[6] = new int[4] { 81, 15, 27, 30 };
-            subjects[7] = new int[4] { 21, 70, 100, 22 };
-            subjects[8] = new int[4] { 88, 50, 13, 12 };
-            subjects[9] = new int[4] { 48, 54, 52, 78 };
-            subjects[10] = new int[4] { 64, 71, 67, 25 };
-            subjects[11] = new int[4] { 16, 93, 46, 72 };
-
-            // Display the array elements:
-            for (int i = 0; i < subjects.Length; i++)
+            // Calculate total for each 
+            for (int i = 0; i < 12; i++)
             {
-                Console.WriteLine("Element {0}", i);
+                int avg = 0;
+
+                for (int j = 0; j < 4; j++)
+                {
+                    avg += Marks[i][j];
+                    
+                }
+                Console.WriteLine("Total marks for each student {0}", avg);
 
             }
+            
+            double[] Avg = new double[12];
 
 
+            // Calculate student average
+            for (int i = 0; i < 12; i++)
+            {
+                double avg = 0.0;
+
+                for (int k = 0; k < 4; k++)
+                {
+                    avg += Marks[i][k];
+                }
+
+                Avg[i] = avg / 4;
+                Console.WriteLine("The average for each student {0}",Avg[i]);
+            }
+            
+            // Calculate subject average
+            Console.WriteLine("Average per subject:");
+
+            for (int m = 0; m < 4; m++)
+            {
+                double subAvg = 0.0;
+                for (int j = 0; j < 12; j++)
+                {
+                    subAvg += Marks[j][m];
+                }
+
+                subAvg /= 12;
+                Console.WriteLine("Subject average {0:0.#####}", subAvg);
+            }
+            return; 
         }
     }
 }
+
---
+++ FOPCSWorskshop/WorkshopG4.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FOPCSWorskshop
+{
+    class WorkshopG4
+    {
+        public static void Main()
+        {
+       
+
+        }
+    }
+}
